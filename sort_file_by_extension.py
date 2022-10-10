41c258f5f42af667eae0c6a79ec761329af777b8
Bartosz Radecki <radecki.b7@gmail.com>
2022-10-10 22:20:13 +0200

    program do sortowania plikow

---
+++ sort_file_by_extension.py
@@ -0,0 +1,68 @@
+def sort_by_ext(files: list[str]) -> list[str]:
+
+    def klucz(file:str):
+
+        name, dot, extension = file.rpartition(".")
+
+        return (extension, name) if name else ("", extension)
+
+
+    return sorted(files, key=klucz)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Example:")
+print(sort_by_ext(["1.cad", "1.bat", "1.aa"]))
+
+assert sort_by_ext(["1.cad", "1.bat", "1.aa"]) == ["1.aa", "1.bat", "1.cad"]
+assert sort_by_ext(["1.cad", "1.bat", "1.aa", "2.bat"]) == [
+    "1.aa",
+    "1.bat",
+    "2.bat",
+    "1.cad",
+]
+assert sort_by_ext(["1.cad", "1.bat", "1.aa", ".bat"]) == [
+    ".bat",
+    "1.aa",
+    "1.bat",
+    "1.cad",
+]
+assert sort_by_ext(["1.cad", "1.bat", ".aa", ".bat"]) == [
+    ".aa",
+    ".bat",
+    "1.bat",
+    "1.cad",
+]
+assert sort_by_ext(["1.cad", "1.", "1.aa"]) == ["1.", "1.aa", "1.cad"]
+assert sort_by_ext(["1.cad", "1.bat", "1.aa", "1.aa.doc"]) == [
+    "1.aa",
+    "1.bat",
+    "1.cad",
+    "1.aa.doc",
+]
+assert sort_by_ext(["1.cad", "1.bat", "1.aa", ".aa.doc"]) == [
+    "1.aa",
+    "1.bat",
+    "1.cad",
+    ".aa.doc",
+]
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
